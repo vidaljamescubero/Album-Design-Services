6fe4bc4920652c0c758c436279f1b29ff91ef13d
vidal james cubero <cubero.vj@gmail.com>
2022-03-15 23:54:53 +0800

    added grid video

--- script.js
+++ script.js
@@ -33,3 +33,52 @@ $.getScript( "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.caro
    
 });
 
+
+document.querySelector(".design-service-block-5-grid-4-button-2").addEventListener("click", function() {
+    
+    let button1 = document.querySelector('.design-service-block-5-grid-4-button-1');
+
+    let button2 = document.querySelector('.design-service-block-5-grid-4-button-2');
+
+    let quote1 = document.querySelector('.design-service-block-5-quote-1');
+
+    let quote2 = document.querySelector('.design-service-block-5-quote-2');
+
+    button1.classList.remove("design-service-block-5-grid-4-button-active");
+    button1.classList.remove("disabled");
+    quote2.classList.remove("design-service-block-5-quote-hidden");
+
+    button2.classList.add("design-service-block-5-grid-4-button-active");
+    button2.classList.add("disabled");
+    quote1.classList.add("design-service-block-5-quote-hidden");
+})
+
+document.querySelector(".design-service-block-5-grid-4-button-1").addEventListener("click", function() {
+    
+    let button1 = document.querySelector('.design-service-block-5-grid-4-button-1');
+
+    let button2 = document.querySelector('.design-service-block-5-grid-4-button-2');
+
+    let quote1 = document.querySelector('.design-service-block-5-quote-1');
+
+    let quote2 = document.querySelector('.design-service-block-5-quote-2');
+
+    button2.classList.remove("design-service-block-5-grid-4-button-active");
+    button2.classList.remove("disabled");
+    quote1.classList.remove("design-service-block-5-quote-hidden");
+
+    button1.classList.add("design-service-block-5-grid-4-button-active");
+    button1.classList.add("disabled");
+    quote2.classList.add("design-service-block-5-quote-hidden");
+     
+})
+
+let accordion = document.querySelectorAll('.design-service-block-8-accordion-container');
+
+for (let i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        console.log("hi")
+      this.classList.toggle('design-service-block-8-accordion-active')
+    })
+  }
+
